97d532880accf573b7b837d5bea1369d60b00f4a
someworkname <145682925+someworkname@users.noreply.github.com>
2024-10-26 13:06:40 +0530

    refactor: reorganize packages

--- js/virtual-glasses.js
+++ js/virtual-glasses.js
@@ -1,6 +1,6 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
-import {OrbitControls} from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
-import {GLTFLoader} from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const webcamElement = document.getElementById('webcam');
 const canvasElement = document.getElementById('canvas');
